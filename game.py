33a043affca44d83895fb71295f4b23fea6424ab
Roman Moskalenko <2016roman00@gmail.com>
2023-10-20 19:46:57 +0300

    add textures

--- game.py
+++ game.py
@@ -8,17 +8,21 @@ from OpenGL.GL.shaders import compileProgram, compileShader
 
 class App:
     def __init__(self):
-        
+
         # initialize python
         pg.init()
         pg.display.set_mode((640, 480), pg.OPENGL | pg.DOUBLEBUF)
         self.clock = pg.time.Clock()
-        
+
         # initialize OpenGl
         glClearColor(0.1, 0.2, 0.2, 1)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         self.shader = self.createShader("shaders/vertex.txt", "shaders/fragment.txt")
         glUseProgram(self.shader)
+        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
         self.triangle = Triangle()
+        self.wood_texture = Material("gfx/wood.jpeg")
         self.mainLoop()
 
     def createShader(self, vertexFilePath, fragmentFilePath):
@@ -37,31 +41,33 @@ class App:
         return shader
 
     def mainLoop(self):
-        
+
         running = True
-        
+
         while running:
             # check events
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-                    
+
             # refresh screen
             glClear(GL_COLOR_BUFFER_BIT)
+
             glUseProgram(self.shader)
+            self.wood_texture.use()
             glBindVertexArray(self.triangle.vao)
             glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex_count)
 
             pg.display.flip()
-            
-            #timing
+
+            # timing
             self.clock.tick(60)
         self.quit()
-    
-    
+
     def quit(self):
 
         self.triangle.destroy()
+        self.wood_texture.destroy()
         glDeleteProgram(self.shader)
         pg.quit()
 
@@ -69,12 +75,11 @@ class App:
 class Triangle:
 
     def __init__(self):
-
         # x, y, z, r, g, b
         self.vertices = (
-            -0.5, -0.5, 0.0, 0.6, 0.3, 0.2,
-             0.5, -0.5, 0.0, 0.0, 0.85, 0.0,
-             0.0,  0.5, 0.0, 0.0, 0.0, 1.0
+            -0.5, -0.5, 0.0, 0.6, 0.3, 0.2, 0.0, 1.0,
+            0.5, -0.5, 0.0, 0.0, 0.85, 0.0, 1.0, 1.0,
+            0.0, 0.5, 0.0, 0.0, 0.0, 1.0, 0.5, 0.0
         )
 
         self.vertices = np.array(self.vertices, dtype=np.float32)
@@ -88,15 +93,39 @@ class Triangle:
         glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
 
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
         glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
+        glEnableVertexAttribArray(2)
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(24))
 
     def destroy(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1, (self.vbo,))
 
-        glDeleteVertexArrays(1, (self.vao, ))
-        glDeleteBuffers(1, (self.vbo, ))
-    
-    
+
+class Material:
+    def __init__(self, filepath):
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+        image = pg.image.load(filepath).convert_alpha()
+        image_width, image_height = image.get_rect().size
+        image_data = pg.image.tostring(image, "RGBA")
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
+        glGenerateMipmap(GL_TEXTURE_2D)
+
+    def use(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+    def destroy(self):
+        glDeleteTextures(1, (self.texture,))
+
+
 if __name__ == "__main__":
     myApp = App()
